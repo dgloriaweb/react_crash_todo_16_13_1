44a05e584db90e91221638057f6747b9734c3fab
unknown <dgloria.web@gmail.com>
2020-03-29 11:31:33 +0100

    tut2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const name = "Josh Perez";
-const element = <h1>Hello {name}</h1>;
+function formatName(user)
+{
+    return user.firstName +  ' ' + user.lastName
+}
+
+const user = {
+    firstName : 'Harper',
+    lastName : 'Perez'
+}
+
+const element = <h1>Hello {formatName(user)}</h1>;
 
 function App() {
   return element;
