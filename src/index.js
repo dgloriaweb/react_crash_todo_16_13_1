4385df7ff088fd6c54e3e5cac50b65cfc5939eff
unknown <dgloria.web@gmail.com>
2020-03-30 10:35:20 +0100

    4_2

--- src/index.js
+++ src/index.js
@@ -1,44 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
 function formatDate(date) {
   return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+      className="Avatar"
+    />
+  );
 }
 
 function Comment(props) {
   return (
     <div className="Comment">
       <div className="UserInfo">
-        <img
-          className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
+        <Avatar user={props.author} />
         <div className="UserInfo-name">
           {props.author.name}
-        </div>
+          </div>
       </div>
-      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-text">
+        {props.text}
+        </div>
       <div className="Comment-date">
         {formatDate(props.date)}
-      </div>
+        </div>
     </div>
   );
 }
 
 const comment = {
   date: new Date(),
-  text: 'I hope you enjoy learning React!',
+  text: "I hope you enjoy learning React!",
   author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'https://placekitten.com/g/64/64',
-  },
+    name: "Hello Kitty",
+    avatarUrl: "https://placekitten.com/g/64/64"
+  }
 };
 ReactDOM.render(
-  <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-  />,
-  document.getElementById('root')
+  <Comment date={comment.date} text={comment.text} author={comment.author} />,
+  document.getElementById("root")
 );
