debb5b2e6756148a8989ca06962a667080cc49a2
unknown <dgloria.web@gmail.com>
2020-03-30 12:02:16 +0100

    5_1

--- src/index.js
+++ src/index.js
@@ -1,45 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+// end of header
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-  return (
-    <img src={props.user.avatarUrl} alt={props.user.name} className="Avatar" />
-  );
-}
-
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">{props.user.name}</div>
-    </div>
-  );
-}
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">{props.text}</div>
-      <div className="Comment-date">{formatDate(props.date)}</div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: "I hope you enjoy learning React!",
-  author: {
-    name: "Hello Kitty",
-    avatarUrl: "https://placekitten.com/g/64/64"
+class Clock extends React.Component {
+  render() {
+    return (
+      <div className="">
+        <h1>Hello World</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}</h2>
+      </div>
+    )
   }
-};
-ReactDOM.render(
-  <Comment date={comment.date} text={comment.text} author={comment.author} />,
-  document.getElementById("root")
-);
+}
+
+function tick() {
+  ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+}
+
+setInterval(tick, 1000);
