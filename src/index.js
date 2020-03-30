91db698d8b4b1e23cc4f2b958534e736ff1c535f
unknown <dgloria.web@gmail.com>
2020-03-30 11:13:55 +0100

    4_3

--- src/index.js
+++ src/index.js
@@ -8,29 +8,25 @@ function formatDate(date) {
 
 function Avatar(props) {
   return (
-    <img
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-      className="Avatar"
-    />
+    <img src={props.user.avatarUrl} alt={props.user.name} className="Avatar" />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
   );
 }
 
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <Avatar user={props.author} />
-        <div className="UserInfo-name">
-          {props.author.name}
-          </div>
-      </div>
-      <div className="Comment-text">
-        {props.text}
-        </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-        </div>
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
     </div>
   );
 }
