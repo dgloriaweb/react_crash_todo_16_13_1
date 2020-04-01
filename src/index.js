3955f2441e414fb3d65ff392d14397adcdc37afc
unknown <dgloria.web@gmail.com>
2020-04-01 10:02:21 +0100

    5_2

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ import "./index.css";
 // end of header
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date:new Date()
+    })
+  }
+
   render() {
     return (
       <div className="">
         <h1>Hello World</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
       </div>
-    )
+    );
   }
 }
 
-function tick() {
-  ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(<Clock />, document.getElementById("root"));
