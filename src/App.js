e37c8c0e03f7b3770694b566ab3d739585738396
unknown <dgloria.web@gmail.com>
2020-04-01 10:30:51 +0100

    fixed Brads crash course stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import "./App.css";
+import Todos from './components/Todos'
+// import { render } from "@testing-library/react";
 
-const comment = ''
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "take trash out",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "walk the dog",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "eat lunch",
+        completed: false
+      }
+    ]
+  };
 
-function App() {
-  return comment
+  render() {
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+// import Clock from './Clock'
+import App from './App'
 // end of header
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date:new Date()
-    })
-  }
-
-  render() {
-    return (
-      <div className="">
-        <h1>Hello World</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Clock />, document.getElementById("root"));
+// ReactDOM.render(<Clock />, document.getElementById("root"));
+ ReactDOM.render(<App />, document.getElementById("root"));
