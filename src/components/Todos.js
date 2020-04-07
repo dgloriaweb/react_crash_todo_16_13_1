867eedff5588eb93f79e630441560ebd879639d6
unknown <dgloria.web@gmail.com>
2020-04-07 10:30:30 +0100

    5_4

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 class Todos extends Component {
   render() {
+    console.log(this.props.todos);
+    
     return (
       <div className="Todos">
         <h1>Todos</h1>
